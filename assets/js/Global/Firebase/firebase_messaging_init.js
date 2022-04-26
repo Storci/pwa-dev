0a8381c2527a5616ebe61016dda889bfb195d6a9
simonezardi <simone.zardi@storci.com>
2022-04-26 11:20:12 +0200

    Update firebase_messaging_init.js

--- assets/js/Global/Firebase/firebase_messaging_init.js
+++ assets/js/Global/Firebase/firebase_messaging_init.js
@@ -1,6 +1,28 @@
 import * as tw from "../Thingworx/thingworx_api_module.js"
 import * as fb from "./firebase_auth_module.js"
 
+navigator.serviceWorker.register('./firebase-messaging-sw.js').then(registration => {
+  firebase.messaging().useServiceWorker(registration)
+
+  const messaging = firebase.messaging()
+
+  messaging.requestPermission()
+  .then(function() {
+    console.log("permission granted")
+    messaging.getToken().then(function(token) {
+      console.log(token)
+      firebase.auth().onAuthStateChanged(user => {
+        tw.service_98_setFirebaseToken(user.email, token)
+      })
+    })
+  })
+  .catch(function(err) {
+    console.warn("No permission to send push: ", err);
+  })
+
+})
+
+/*
 const messaging = firebase.messaging()
 
 messaging.requestPermission()
@@ -16,3 +38,4 @@ messaging.requestPermission()
 .catch(function(err) {
   console.warn("No permission to send push: ", err);
 })
+*/
