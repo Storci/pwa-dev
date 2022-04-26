72af344a5eda31937559f2d23406eb7619057fe4
simonezardi <simone.zardi@storci.com>
2022-04-26 11:31:58 +0200

    Update 01_Customers.html

--- assets/js/Global/Firebase/firebase_messaging_init.js
+++ assets/js/Global/Firebase/firebase_messaging_init.js
@@ -21,21 +21,3 @@ navigator.serviceWorker.register('./firebase-messaging-sw.js').then(registration
   })
 
 })
-
-/*
-const messaging = firebase.messaging()
-
-messaging.requestPermission()
-.then(function() {
-  console.log("permission granted")
-  messaging.getToken().then(function(token) {
-    console.log(token)
-    firebase.auth().onAuthStateChanged(user => {
-      tw.service_98_setFirebaseToken(user.email, token)
-    })
-  })
-})
-.catch(function(err) {
-  console.warn("No permission to send push: ", err);
-})
-*/
