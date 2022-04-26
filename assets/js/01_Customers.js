4c466374b4141e6cb4c76a88becfd019c0383f30
simonezardi <83826210+simonezardi@users.noreply.github.com>
2022-04-26 13:46:39 +0200

    PWA-19 Sistemato cambio pagina (#49)

--- assets/js/01_Customers.js
+++ assets/js/01_Customers.js
@@ -99,9 +99,7 @@ function createCard(customerList){
 			//localStorage.setItem('global_selected_customer', customerName)
 			localStorage.setItem('global_entityName', el.name)
 			// Carica la pagina.
-			//window.location.href = "./02_Dashboard.html?entityName=" + el.name
-      let href = "./02_Dashboard.html?entityName=" + el.name
-      $('#iframe_id', parent.document).attr('src', href)
+			window.location.href = "./02_Dashboard.html?entityName=" + el.name
 		}
 	})
 }
