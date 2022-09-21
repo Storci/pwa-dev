b187c35bdb00471524a46e973914eb98d1fb63a6
richsa <83827987+richsa@users.noreply.github.com>
2022-09-21 17:33:24 +0200

    apportate modifche

--- assets/js/92_alarmHistory.js
+++ assets/js/92_alarmHistory.js
@@ -5,124 +5,112 @@ import * as fb from "./Global/Firebase/firebase_auth_module.js"
 import * as lang from "./Global/Common/Translation.js"
 import * as common from "./Global/Common/commonFunctions.js"
 
-
-
-/*async function getAlarmsNotifications(days, filter, getHistory){
-    let alarmResponse = await tw.getListAlert(days, filter, getHistory)
-    console.log(alarmResponse)
-}*/
-
-
-
 // Recupera il nome dell'utente da firebase, controlla che sia loggato.
 // Nel caso non fosse loggato richiama la pagina di login
 fb.onAuthStateChanged_2()
 // funzione per la traduzione 
 lang.getLanguage()
 
+// Funzione che ritorna l'anno il mese e il giorno
+// partendo da una data completa.
+// Il formato è YYYY-MM-DD
+function getDate(date){
+  // Recupera l'anno
+	let year  = date.getFullYear()
+  // Recupera il numero del mese - da 0 a 11
+	let month = date.getMonth()
+  // Recupera il giorno del mese - da 1 a 31
+	let day   = date.getDate()
+  // Converte la variabile month in number e ci somma 1
+  // per portare il range da 1 a 12
+	month = Number(month) + 1
+  // Aggiunge lo zero nei numeri inferiori a 10, in modo da avere sempre 2 cifre
+	month = month < 10 ? '0' + month : month
+  // Aggiunge lo zero nei numeri inferiori a 10, in modo da avere sempre 2 cifre
+  day = day < 10 ? '0' + day : day
+  // Ritorna la stringa
+	return year + '-' + month + '-' + day
+}
+
 //funzione per recuperare i dati da tw per mettere nella tabella
-function getAlarmsNotifications(startDate, endDate, filter, getHistory){
+function getAlarmsNotifications(idTable, startDate, endDate, filter, getHistory){
     tw.getListAlert(startDate, endDate, filter, getHistory)
     .then((list)=>{
         console.log(list)
-    $("#IDAlertHistoryBody")
+    $(idTable).empty()
 
-        list.rows.forEach(el =>{
+        list.rows.forEach((el,i) =>{
           let timeStart = new Date(el.TimeStart).toLocaleString();
           let timeEnd = new Date(el.TimeStart).toLocaleString();
-          let row = '<tr id=' + ' class="hover_tr" style="border-style: none;background: var(--bs-table-bg);">'
+          // Definisci l' id della riga della tabella
+          let id = "IDHistoryTableRow" + i;
+          let row = '<tr id=' + id + ' class="hover_tr" style="border-style: none;background: var(--bs-table-bg);">'
           row    += '    <td style="font-size: 12px;border-style: none;">' + el.CustomerName  + '</td>'
           row    += '    <td style="font-size: 12px;border-style: none;">' + el.MachineName    + '</td>'
           row    += '    <td style="font-size: 12px;border-style: none;">' +  el.Gravity + '</td>'
           row    += '    <td style="font-size: 12px;border-style: none;">' + timeStart + '</td>'
           row    += '    <td style="font-size: 12px;border-style: none;">' + timeEnd + '</td>'
           row    += '    <td style="font-size: 12px;border-style: none;">' +el.Message  + '</td>'
-          row    += '</tr>'
+          row    += '</tr>'  
 				// Aggiunge la riga alla tabella
-				$('#IDAlertHistoryBody').append(row);
+				$(idTable).append(row);
         })
-       /*for(let i=0; i < list.length; i++){
-        console.log(list[i].CustomerName)
-       }*/
     })
     .catch((err)=>{
         console.log('promise rejected', err)
     })
-}
+    /*function searchTable(value, data){
+      var filterData = list
+      console.log(filterData)
+      for (var i = 0; i < data.length;i++){
 
+      }
 
-
-var inputStart = "15/09/2022";
-var inputEnd   = "18/09/2022";
-
-
-  const alarmi = getAlarmsNotifications(new Date (inputStart),new Date (inputEnd),"*", true);
-
-
-
-
-
-
-
-
-/*var $table = $('#fresh-table')
-var $alertBtn = $('#alertBtn')
-
-window.operateEvents = {
-  'click .like': function (e, value, row, index) {
-    alert('You click like icon, row: ' + JSON.stringify(row))
-    console.log(value, row, index)
-  },
-  'click .edit': function (e, value, row, index) {
-    alert('You click edit icon, row: ' + JSON.stringify(row))
-    console.log(value, row, index)
-  },
-  'click .remove': function (e, value, row, index) {
-    $table.bootstrapTable('remove', {
-      field: 'id',
-      values: [row.id]
-    })
-  }
-}
-
-function operateFormatter(value, row, index) {
-  return [
-    '<a rel="tooltip" title="Like" class="table-action like" href="javascript:void(0)" title="Like">',
-      '<i class="fa fa-heart"></i>',
-    '</a>',
-    '<a rel="tooltip" title="Edit" class="table-action edit" href="javascript:void(0)" title="Edit">',
-      '<i class="fa fa-edit"></i>',
-    '</a>',
-    '<a rel="tooltip" title="Remove" class="table-action remove" href="javascript:void(0)" title="Remove">',
-      '<i class="fa fa-remove"></i>',
-    '</a>'
-  ].join('')
-}
-
-$(function () {
-  $table.bootstrapTable({
-    classes: 'table table-hover table-striped',
-    toolbar: '.toolbar',
-
-    search: true,
-    showRefresh: true,
-    showToggle: true,
-    showColumns: true,
-    pagination: true,
-    striped: true,
-    sortable: true,
-    pageSize: 8,
-    pageList: [8, 10, 25, 50, 100],
-
-    formatShowingRows: function (pageFrom, pageTo, totalRows) {
-      return ''
-    },
-    formatRecordsPerPage: function (pageNumber) {
-      return pageNumber + ' rows visible'
     }
-  })
+    searchTable();*/
+}
 
-  $alertBtn.click(function () {
-    alert('You pressed on Alert')
+function alertHistory(){
+  let alertStartHistory = new Date()
+  let alertEndHistory   = new Date()
+
+  // imposta 30 giorni prima della data odierna
+  alertStartHistory.setDate(alertStartHistory.getDate()-9)
+  // Imposta i 2 data picker con le date calcolate prima
+	// La funzione getDate ritorna solamente l'anno, il mese e il giorno
+	// yyyy-MM-dd
+  $('#IDtimeStart').val(getDate(alertStartHistory))
+  $('#IDtimeEnd').val(getDate(alertEndHistory))
+
+  getAlarmsNotifications('#IDAlertHistoryBody',alertStartHistory,alertEndHistory,"*",true)
+
+  
+$("#IDtimeStart").change(function(){
+  // Recupera i valori di inizio e fine produzione
+  let alertStartHistory = new Date($(this).val())
+  let alertEndHistory   = new Date($('#IDtimeStart').val())
+  // Recupera la lista delle produzioni
+  // Per default viene visualizzata la prima produzione dell'elenco. (l'ultima produzione effettuata in ordine cronologico)
+   getAlarmsNotifications('#IDAlertHistoryBody',alertStartHistory,alertEndHistory,"*", true);
   })
-})*/
+  
+$("#IDtimeEnd").change(function(){
+    // Recupera i valori di inizio e fine produzione
+    let alertStartHistory = new Date($('#IDtimeEnd').val())
+    let alertEndHistory   = new Date($(this).val())
+    // Recupera la lista delle produzioni
+    // Per default viene visualizzata la prima produzione dell'elenco. (l'ultima produzione effettuata in ordine cronologico)
+     getAlarmsNotifications('#IDAlertHistoryBody',alertStartHistory,alertEndHistory,"*", true);
+  })
+}
+alertHistory();
+  
+$("#IDsearch").on('keyup', function(){
+  let inputSearch = $(this).val()
+  console.log(inputSearch)
+})
+
+/*function searchTable(value, data){
+
+
+}*/
