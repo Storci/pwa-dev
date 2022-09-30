2a903f29eeb9a39302627a4c7857bb6000e9bb56
simonezardi <simone.zardi@storci.com>
2022-09-30 11:07:42 +0200

    Aggiunto spinner pagine allarmi

--- assets/js/95_actualAlarm.js
+++ assets/js/95_actualAlarm.js
@@ -6,20 +6,19 @@ import * as lang from "./Global/Common/Translation.js"
 import * as common from "./Global/Common/commonFunctions.js"
 
 
-
 // Recupera il nome dell'utente da firebase, controlla che sia loggato.
 // Nel caso non fosse loggato richiama la pagina di login
 fb.onAuthStateChanged_2()
-// funzione per la traduzione 
+// funzione per la traduzione
 lang.getLanguage()
 
 
 let customerName = localStorage.getItem('global_customer')
-console.log(customerName)
 
 // Cancella tutte le righe della tabella
 $("#IDAlertActualBody").empty()
 
+// Sorting function
 let direction = true
 $("th").click(function() {
 	let icon = "#" + $(this)[0].children[0].children[1].id
@@ -35,13 +34,20 @@ $("th").click(function() {
 	let column = $(this).index()
 	let table = $("#IDAlertActualBody")
 
-	//let start = new Date().getTime()
 	insertionSort(table[0], column, direction)
-	//let stop  = new Date().getTime()
-	//console.log(stop-start + " ms")
 
 	direction = !direction
 })
+
+
+$('#modal1').modal("show")
+// richiamo della funzione
+getAlarmsNotifications("*", false,customerName);
+
+
+// *******************************************
+// ************** FUNZIONI *******************
+// *******************************************
 
 function convertDate(s){
 	let sdate = s
@@ -57,6 +63,7 @@ function convertDate(s){
 	return Date.parse(year + "/" + month + "/" + day + " " + time)
 }
 
+// Funzione per il riordino delle colonne
 function insertionSort(table, column, dir){
 	let rows = 	table.children
 	let parent = table
@@ -83,32 +90,38 @@ function insertionSort(table, column, dir){
 		}
 	}
 }
-
-//funzione per recuperare i dati da tw per mettere nella tabella
+// Funzione per recuperare i dati da tw per mettere nella tabella
 function getAlarmsNotifications(filter, getHistory,customerName){
-    tw.getListAlert("","",filter, getHistory,customerName)
-    .then((list)=>{
-        console.log(list)
-    $("#IDAlertActualBody").empty()
-        list.rows.forEach(el =>{
-            let timeStart = new Date(el.TimeStart).toLocaleString();
-        let row = '<tr class="alert" role="alert">'
-				row    += '    <td >' + timeStart  + '</td>'
-				row    += '    <td >' + el.MachineName    + '</td>'
-				row    += '    <td >' + el.Gravity + '</td>'
-				row    += '    <td >' + el.CustomerName  + '</td>'
-				row    += '    <td >' + el.Message  + '</td>'
-				row    += '</tr>'
-				// Aggiunge la riga alla tabella
-				$('#IDAlertActualBody').append(row);
-        })
-       /*for(let i=0; i < list.length; i++){
-        console.log(list[i].CustomerName)
-       }*/
-    })
-    .catch((err)=>{
-        console.log('promise rejected', err)
-    })
+	tw.getListAlert("","",filter, getHistory,customerName)
+	.then((list)=>{
+		$("#IDAlertActualBody").empty()
+		if(list.rows.length == 0){
+			let row = '<tr class="alert" role="alert">'
+			row    += '    <td ></td>'
+			row    += '    <td ></td>'
+			row    += '    <td ></td>'
+			row    += '    <td ></td>'
+			row    += '    <td >Nessun allarme trovato</td>'
+			row    += '</tr>'
+			// Aggiunge la riga alla tabella
+			$('#IDAlertActualBody').append(row);
+		}
+
+		list.rows.forEach(el =>{
+			let timeStart = new Date(el.TimeStart).toLocaleString();
+			let row = '<tr class="alert" role="alert">'
+			row    += '    <td >' + timeStart  + '</td>'
+			row    += '    <td >' + el.MachineName    + '</td>'
+			row    += '    <td >' + el.Gravity + '</td>'
+			row    += '    <td >' + el.CustomerName  + '</td>'
+			row    += '    <td >' + el.Message  + '</td>'
+			row    += '</tr>'
+			// Aggiunge la riga alla tabella
+			$('#IDAlertActualBody').append(row);
+		})
+		$('#modal1').modal("hide")
+	})
+	.catch((err)=>{
+		console.log('promise rejected', err)
+	})
 }
-// richiamo della funzione
- getAlarmsNotifications("*", false,customerName);
