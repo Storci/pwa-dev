b636d34a44ecf6afbd969656ab5563edcb9a9a04
simonezardi <83826210+simonezardi@users.noreply.github.com>
2022-11-08 10:52:11 +0100

    PWA-79 pagina report allarmi (#138)
    * Create 97_reportAlarms.html
    
    * Modificato pagina 97
    
    Aggiunto gli elementi della pagina.
    modificato il js relativo.
    
    aggiornato la sidebar per puntare alla nuova pagina
    
    * Aggiunto date time picker nella pagina 97
    
    Aggiunto lo scroll nella sidebar

--- assets/js/97_reportAlarms.js
+++ assets/js/97_reportAlarms.js
@@ -11,18 +11,95 @@ fb.onAuthStateChanged_2()
 // funzione per la traduzione
 lang.getLanguage()
 
+// Definisce le variabili come date
+let timeStartHistory = new Date()
+let timeEndHistory   = new Date()
+// Imposta X giorni prima della data odierna
+timeStartHistory.setDate(timeStartHistory.getDate() - 2)
+
 let customerName = localStorage.getItem('global_customer')
+let query = {
+    "filters":{
+        "type": "and",
+        "filters":[{
+            "type":"Between",
+            "fieldName":"timestamp",
+            "from": timeStartHistory.getTime(),
+            "to": timeEndHistory.getTime()
+        }
+        ]
+    }
+}
 
-getAlertsReport()
+if(customerName !== 'Storci'){
+    let customerQuery = {
+        "type":"EQ",
+        "fieldName":"CustomerName",
+        "value": customerName
+    }
+    query.filters.filters.push(customerQuery)
+}
+console.log(query)
+
+getAlertsReport(query)
 
 
+// Imposta i 2 data picker con le date calcolate prima
+// La funzione getDate ritorna solamente l'anno, il mese e il giorno
+// yyyy-MM-dd
+let disp_timeStart = common.getDate(timeStartHistory)
+let disp_timeEnd = common.getDate(timeEndHistory)
 
+$('#dateFilter').daterangepicker({
+    "locale": {
+        "format": "YYYY/MM/DD",
+        "separator": " - ",
+        "applyLabel": "Apply",
+        "cancelLabel": "Cancel",
+        "fromLabel": "From",
+        "toLabel": "To",
+        "customRangeLabel": "Custom",
+        "weekLabel": "W",
+        "daysOfWeek": [
+            "Su",
+            "Mo",
+            "Tu",
+            "We",
+            "Th",
+            "Fr",
+            "Sa"
+        ],
+        "monthNames": [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ],
+        "firstDay": 1
+    },
+    "startDate": disp_timeStart,
+    "endDate": disp_timeEnd
+}, function(start, end, label) {
+    query.filters.filters[0].from = start._d.getTime();
+    query.filters.filters[0].to = end._d.getTime();
+
+    getAlertsReport(query)
+})
 
 // *******************************************
 // ************** FUNZIONI *******************
 // *******************************************
 
 function getAlertsReport(query){
+    $('#accordion').empty()
     // Recupera la lista di tutti gli allarmi, filtrati dalla query
     tw.service_11_AlertsReport(query)
     .then(list=>{
@@ -48,7 +125,7 @@ function getAlertsReport(query){
             const idCollapse     = machine
             const idCollapseBody = machine + '-collapse'
             const idAccordion    = machine + '-accordion'
-            
+
             let accordion = ''
             accordion += '<div class="accordion-item">'
             accordion +=     '<h2 class="accordion-header" id="' + idCollapse + '">'
@@ -60,56 +137,58 @@ function getAlertsReport(query){
             accordion +=        '</button>'
             accordion +=     '</h2>'
             accordion +=     '<div id="' + idCollapseBody + '" class="accordion-collapse collapse" aria-labelledby="' + idCollapse + '" data-bs-parent="#accordion">'
-            accordion +=         '<div class="accordion-body pe-0">'
-            accordion +=             '<div class="accordion accordion-flush" id="' + idAccordion + '"></div>' 
-            accordion +=         '</div>'                                       
-            accordion +=     '</div>'                             
-            accordion += '</div>'  
+            accordion +=         '<div class="accordion-body pt-0 pb-0 pe-0">'
+            accordion +=             '<div class="accordion accordion-flush" id="' + idAccordion + '"></div>'
+            accordion +=         '</div>'
+            accordion +=     '</div>'
+            accordion += '</div>'
 
             $('#accordion').append(accordion)
 
             customers.forEach(customer => {
                 const customerAlertCount = machineAlertCount.filter(el => el.CustomerName === customer)
-                const alarms = [...new Set(customerAlertCount.map(x => x.ID_AlertTable))]              
+                const alarms = [...new Set(customerAlertCount.map(x => x.ID_AlertTable))]
 
-                const idCustomerCollapse     = idCollapse + '-' + customer.replace(/ /g, '_')
-                const idCustomerCollapseBody = idCollapse + '-' + customer.replace(/ /g, '_') + '-collapse'
-                const idCustomerList         = idCollapse + '-' + customer.replace(/ /g, '_') + '-list'
-                const idCustomerAccordion    = '#' + idAccordion
+                if(alarms.length > 0){
+                    const idCustomerCollapse     = idCollapse + '-' + customer.replace(/ /g, '_')
+                    const idCustomerCollapseBody = idCollapse + '-' + customer.replace(/ /g, '_') + '-collapse'
+                    const idCustomerList         = idCollapse + '-' + customer.replace(/ /g, '_') + '-list'
+                    const idCustomerAccordion    = '#' + idAccordion
 
-                let accordionCustomer = ''
-                accordionCustomer += '<div class="accordion-item">'
-                accordionCustomer +=     '<h2 class="accordion-header" id="' + idCustomerCollapse + '">'
-                accordionCustomer +=        '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#' + idCustomerCollapseBody + '" aria-expanded="false" aria-controls="' + idCustomerCollapseBody + '">'
-                accordionCustomer +=            '<div class="d-flex justify-content-between me-5 w-100" data-pg-collapsed>'
-                accordionCustomer +=                '<label>' + customer + '</label>'
-                accordionCustomer +=                '<label>' + customerAlertCount.length + '</label>'
-                accordionCustomer +=            '</div>'
-                accordionCustomer +=        '</button>'
-                accordionCustomer +=     '</h2>'
-                accordionCustomer +=     '<div id="' + idCustomerCollapseBody + '" class="accordion-collapse collapse" aria-labelledby="' + idCustomerCollapse + '" data-bs-parent="' + idCustomerAccordion + '">'
-                accordionCustomer +=         '<div class="accordion-body pe-0">'
-                accordionCustomer +=             '<ul id="' + idCustomerList + '" class="list-group list-group-flush"></ul>' 
-                accordionCustomer +=         '</div>'                                       
-                accordionCustomer +=     '</div>'                             
-                accordionCustomer += '</div>'  
+                    let accordionCustomer = ''
+                    accordionCustomer += '<div class="accordion-item">'
+                    accordionCustomer +=     '<h2 class="accordion-header" id="' + idCustomerCollapse + '">'
+                    accordionCustomer +=        '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#' + idCustomerCollapseBody + '" aria-expanded="false" aria-controls="' + idCustomerCollapseBody + '">'
+                    accordionCustomer +=            '<div class="d-flex justify-content-between me-5 w-100" data-pg-collapsed>'
+                    accordionCustomer +=                '<label>' + customer + '</label>'
+                    accordionCustomer +=                '<label>' + customerAlertCount.length + '</label>'
+                    accordionCustomer +=            '</div>'
+                    accordionCustomer +=        '</button>'
+                    accordionCustomer +=     '</h2>'
+                    accordionCustomer +=     '<div id="' + idCustomerCollapseBody + '" class="accordion-collapse collapse" aria-labelledby="' + idCustomerCollapse + '" data-bs-parent="' + idCustomerAccordion + '">'
+                    accordionCustomer +=         '<div class="accordion-body pt-0 pb-0 pe-0">'
+                    accordionCustomer +=             '<ul id="' + idCustomerList + '" class="list-group list-group-flush"></ul>'
+                    accordionCustomer +=         '</div>'
+                    accordionCustomer +=     '</div>'
+                    accordionCustomer += '</div>'
 
-                $(idCustomerAccordion).append(accordionCustomer)
+                    $(idCustomerAccordion).append(accordionCustomer)
 
-                alarms.forEach(alarm => {
-                    const AlertCount = customerAlertCount.filter(el => el.ID_AlertTable === alarm)
-                    const id = '#' + idCustomerList
+                    alarms.forEach(alarm => {
+                        const AlertCount = customerAlertCount.filter(el => el.ID_AlertTable === alarm)
+                        const id = '#' + idCustomerList
 
-                    let list = ''
-                    list += '<li class="d-flex justify-content-between list-group-item">'
-                    list +=     '<label>' + AlertCount[0].Message + '</label>'
-                    list +=     '<span class="badge bg-danger rounded-pill">' + AlertCount.length + '</span>'
-                    list += '</li> '
+                        let list = ''
+                        list += '<li class="d-flex justify-content-between list-group-item">'
+                        list +=     '<label>' + AlertCount[0].Message + '</label>'
+                        list +=     '<span class="badge bg-danger rounded-pill">' + AlertCount.length + '</span>'
+                        list += '</li> '
 
-                    $(id).append(list)
-                })
-                    
+                        $(id).append(list)
+                    })
+                }
+
             })
-        })       
+        })
     })
 }
