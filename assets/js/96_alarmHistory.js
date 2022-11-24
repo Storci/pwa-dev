2692eb1a4874a9c054a0f337f340138fd37ca5d0
simonezardi <simone.zardi@storci.com>
2022-11-24 12:23:49 +0100

    Update 96_alarmHistory.js

--- assets/js/96_alarmHistory.js
+++ assets/js/96_alarmHistory.js
@@ -185,6 +185,7 @@ function insertionSort(table, column, dir){
 
 // Funzione per recuperare i dati da tw per mettere nella tabella
 function getAlarmsNotifications(idTable, startDate, endDate, filter, getHistory, customerName){
+	customerName = customerName.replace(/_/g, ' ')
 	tw.service_10_getAlerts(startDate, endDate, filter, getHistory,customerName)
 	.then((list)=>{
     setTimeout(function() {	$('#modal1').modal("hide") }, 500);
