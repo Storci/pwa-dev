06c59cd7eb83525372eb2d45fc7848591537ebe9
richsa <83827987+richsa@users.noreply.github.com>
2024-10-31 17:19:32 +0100

    apportate modifiche
    -aggiornato la pagina storico delle linea
    -aggiunto la pagina con informazione della pagina line_stacker e Line_sestacker

--- assets/js/Global/Thingworx/thingworx_api_module.js
+++ assets/js/Global/Thingworx/thingworx_api_module.js
@@ -705,6 +705,44 @@ function getLineNidiLasagnaInfo(entityName) {
     });
 }
 
+// Fetch API 
+//la funzione recupera informazione dall'Impilatore
+function getLineImpilatoreInfo(entityName){
+    let url = baseUrl + "Things/Storci.Thing.Manage.Bootstrap/Services/getLineImpilatoreInfo";
+    let data = JSON.stringify({"entityName":entityName})
+
+    return fetch(url, {
+        method: 'POST', 
+        headers : headers,
+        body :data
+    })
+    .then(response=>{
+        if (!response.ok){
+            throw new Error("Network response was not ok" + response.statusText)
+        }
+        return response.json()
+    })
+}
+
+// Fetch API 
+//la funzione recupera informazione dal robot deimpilatore
+function getLineDeimpilatoreInfo(entityName){
+    let url = baseUrl + "Things/Storci.Thing.Manage.Bootstrap/Services/getLineDeimpilatoreInfo";
+    let data = JSON.stringify({"entityName":entityName})
+
+    return fetch(url, {
+        method: 'POST', 
+        headers : headers,
+        body :data
+    })
+    .then(response=>{
+        if (!response.ok){
+            throw new Error("Network response was not ok" + response.statusText)
+        }
+        return response.json()
+    })
+}
+
 
 // Fetch API
 // Recupera la produzione storica dell'impasto
@@ -901,5 +939,7 @@ export{
 	getDryerTimeRange,
 	getLineTimeRange,
     calculateConsumo,
+    getLineImpilatoreInfo,
+    getLineDeimpilatoreInfo,
     calculateConsumoImpasto
 }
